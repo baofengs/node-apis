fddce778d792173c7ac587a3d11d02e10f55b68b
sanbaofeng <sanbaofeng@hetao101.com>
2023-12-08 17:16:18 +0800

    chore: update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'node apis in vercel';
+    return 'node apis in vercel~';
   }
 
   getJson() {
